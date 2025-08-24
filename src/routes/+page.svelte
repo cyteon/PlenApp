4a83f1a61be5fe182c895371dc63e3c04cde3edf
cyteon <cyteon@hackclub.app>
2025-08-24 21:55:21 +0200

    plane stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -96,7 +96,66 @@
 
     {#if searched}
       {#if result}
-        test
+        {#if result.aircraft}
+          <div class="px-4 pt-2 pb-4 rounded-md bg-ctp-mantle mt-2 border">
+            <div class="flex">
+              <h2 class="text-xl font-semibold">{result.aircraft.registration}</h2>
+              <p class="text-lg ml-auto">
+                {result.aircraft.manufacturer} {result.aircraft.type}
+              </p>
+            </div>
+            
+            <p class="text-lg mt-1">
+              Operated by {result.aircraft.registered_owner} ({result.aircraft.registered_owner_operator_flag_code})
+            </p>
+
+            <p class="text-lg">
+              Registered in {result.aircraft.registered_owner_country_name} ({result.aircraft.registered_owner_country_iso_name})
+            </p>
+
+            <img 
+              src={result.aircraft.url_photo} 
+              alt="Aircraft_Image" 
+              class="mt-2 rounded-md w-full h-auto"
+            />
+          </div>
+        {/if}
+
+        {#if result.callsign}
+          <div class="px-4 pt-2 pb-4 rounded-md bg-ctp-mantle mt-2 border">
+            <h2 class="text-xl font-semibold">{result.callsign.callsign_icao} / {result.callsign.callsign_iata || "no iata"}</h2>
+            
+            <p class="text-lg mt-1">
+              Operated by {result.callsign.airline.name} ({result.callsign.airline.iata || "no iata"} / {result.callsign.airline.icao || "no icao"})
+            </p>
+
+            <p class="text-lg">
+              Origin: {result.callsign.origin.name} ({result.callsign.origin.iata_code || "no iata"} / {result.callsign.origin.icao_code || "no icao"})
+            </p>
+
+            <p class="text-lg">
+              Destination: {result.callsign.destination.name} ({result.callsign.destination.iata_code || "no iata"} / {result.callsign.destination.icao_code || "no icao"})
+            </p>
+          </div>
+        {/if}
+
+        {#if result.airlines.length > 0}
+          <div class="px-4 pt-2 pb-4 rounded-md bg-ctp-mantle mt-2 border">
+            <h2 class="text-xl font-semibold">Airlines</h2>
+            
+            {#each result.airlines as airline}
+              <div class="p-2 border rounded-md mt-2">
+                <h3 class="font-semibold">{airline.name} {
+                  airline.iata && airline.icao ? `(${airline.iata} / ${airline.icao})` : (
+                    airline.iata ? `(${airline.iata})` : (
+                      airline.icao ? `(${airline.icao})` : ""
+                    )
+                  )
+                }</h3>
+              </div>
+            {/each}
+          </div>
+        {/if}
       {:else if !searchFinished}
         <p class="mx-auto mt-8">Searching...</p>
       {:else}
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,9 +1,80 @@
-<script>
-    import Navbar from "$lib/ui/navbar.svelte";
+<script lang="ts">
+  import Navbar from "$lib/ui/navbar.svelte";
+  import { onMount } from "svelte";
+  import storePromise from "$lib/store";
+  
+  import TileLayer from "ol/layer/Tile";
+  import OSM from 'ol/source/OSM';
+  import VectorLayer from 'ol/layer/Vector';
+  import VectorSource from "ol/source/Vector";
+  import { fromLonLat } from "ol/proj";
+  import { Feature, Map, View } from "ol";
+  import { Point } from "ol/geom";
+  import "ol/ol.css";
+    import Style from "ol/style/Style";
+    import Icon from "ol/style/Icon";
+    import generatePlaneIcon from "$lib/generatePlaneIcon";
+
+  let planes: any[] = [];
+
+  let mapContainer: HTMLDivElement;
+
+  onMount(async () => {
+    try {
+      let token = await (await storePromise).get("token");
+      
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/app/flights/all`, {
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch planes");
+      }
+
+      planes = await res.json();
+    } catch (err) {
+      console.error("Error fetching planes:", err);
+    }
+
+    const osmLayer = new TileLayer({
+      source: new OSM(),
+    });
+
+    const vectorLayer: any = new VectorLayer({
+      source: new VectorSource(),
+    });
+
+    const map = new Map({
+      target: mapContainer,
+      layers: [osmLayer, vectorLayer],
+      view: new View({
+        center: fromLonLat([0, 0]),
+        zoom: 2,
+      }),
+    });
+
+    planes.forEach(plane => {
+      const feature = new Feature({
+        geometry: new Point(fromLonLat([plane.longitude, plane.latitude])),
+        name: plane.callsign,
+      });
+
+      feature.setStyle(new Style({
+        image: new Icon({
+          src: generatePlaneIcon(plane.category),
+          rotation: plane.trueTrack ? ((plane.trueTrack * Math.PI) / 180) : 0,
+        }),
+      }));
+
+      vectorLayer.getSource().addFeature(feature);
+    });
+  });
 </script>
 
 <main class="flex flex-col flex-1">
-  you wish
+  <div class="flex flex-1" bind:this={mapContainer}></div>
 
   <div class="mt-auto">
     <Navbar />
